5692a49f3d076db59c41eeafe7c1926cd300d0a5
Nassim Chantout <chantoutnassim@gmail.com>
2024-12-05 11:25:11 +0100

    Main Base

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-red-600 underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default NavBar
