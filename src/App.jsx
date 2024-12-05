cb8378350bf0ca9808411d8fd2ac2289f46baecb
Nassim Chantout <chantoutnassim@gmail.com>
2024-12-05 12:39:34 +0100

    Add react-router-dom for routing and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 
 function App() {
-
   return (
-    <>
-      <NavBar />
-    </>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/NavBar" element={<NavBar />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App
