e26ac0280df6bc72de0eb8fdd2d04350a3488b61
Muhammad Shahid <contact@mshahid.fr>
2024-12-11 11:47:26 +0100

    Ajouté Popup.jsx et modifié Formulaire.jsx

---
+++ src/components/Popup.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Popup({
+  message = "Message envoyé avec succès",
+    position = "top-20 left-2", 
+    backgroundColor = "bg-green-500", 
+    textColor = "text-white", 
+    borderRadius = "rounded-md", 
+    animation = "fadein 0.5s", 
+    }) {
+    const popupStyle = {
+        animation, 
+    };
+
+  const fadeinKeyframes = `
+    @keyframes fadein {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+  `;
+
+  return (
+    <>
+      <style>{fadeinKeyframes}</style>
+      <div
+        className={`fixed ${position} p-4 z-50 ${backgroundColor} ${textColor} ${borderRadius}`}
+        style={popupStyle}
+      >
+        <p>{message}</p>
+      </div>
+    </>
+  );
+}
+
+export default Popup;
