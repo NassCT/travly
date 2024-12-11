6695d49e55b1ee7e7cba716144009cc819ba9e21
CecileYhy <gastrin.ce@gmail.com>
2024-12-11 17:24:14 +0100

    Footer maj sans bonnes icones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,34 +4,41 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className='bg-colorB text-colorW py-10 text-center'>
-      <div>
+    <footer className='bg-colorB text-colorW py-10 text-center '>
+      <div className='grid grid-cols-2 justify-center gap-4 md:grid-cols-2 lg:flex'>
         <Link
-        to="/mentions-legales"
-        className='text-sm mx-4 hover:underline'>
+          to="/MentionsLegales"
+          className='text-sm sm:text-base hover:underline'>
           Mentions légales
         </Link>
         <Link
-        to="/mentions-legales"
-        className='text-sm mx-4 hover:underline'>
+          to="/Confidentialites"
+          className='text-sm sm:text-base hover:underline'>
           Politique de confidentialités
         </Link>
         <Link
-        to="/mentions-legales"
-        className='text-sm mx-4 hover:underline'>
+          to="/Conditions"
+          className='text-sm sm:text-base hover:underline'>
           Conditions d'utilisation
         </Link>
         <Link
-        to="/mentions-legales"
-        className='text-sm mx-4 hover:underline'>
+          to="/Contact"
+          className='text-sm sm:text-base hover:underline'>
           Contact
         </Link>
       </div>
-      <div className='text-xs mt-6'>
+      <div className='flex justify-center mt-6 gap-4'>
+        <div className='flex gap-2'>
+          <a href="https://facebook.com" className="text-xl">🔵</a>
+          <a href="https://instagram.com" className="text-xl">📸</a>
+          <a href="https://twitter.com" className="text-xl">🐦</a>
+        </div>
+        <div className='text-xs mt-2'>
         © Travely - 2024
+        </div>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
