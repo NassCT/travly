7abeec39eb471e41e887d1d1488ce4f87eb3e65b
CecileYhy <gastrin.ce@gmail.com>
2025-01-09 10:05:21 +0100

    Bannière Accueil responsive + ccessibilité ok

--- src/components/Banniere.jsx
+++ src/components/Banniere.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import Buttons from './Buttons';
 
 function Banniere() {
   return (
-    <section className="relative py-28">
+    <section className="relative p-6">
 
     <div className="relative max-w-6xl mx-auto">
 
         <img 
           src="./src/assets/images/viewPlane.webp" 
-          alt="Bannière de voyage"
-          className="w-full h-80 object-cover rounded-xl" 
+          alt="Image avec une vue d'un avion au-dessus des nuages, symbolisant un voyage"
+          className="w-full h-60 md:h-80 object-cover rounded-xl" 
         />
 
-        <div className="absolute inset-0 bg-colorB opacity-50 rounded-xl z-0"></div>
+        <div className="absolute inset-0 bg-colorB opacity-50 rounded-xl z-0"
+        aria-hidden="true"
+        ></div>
 
-        <div className="absolute inset-0 flex flex-col justify-center items-start text-start text-white ml-10">
-          <h2 className="text-3xl font-bold mb-4">Allez plus loin, rapprochez-vous</h2>
-          <p className="text-lg mb-6">Éclipsez-vous pour un séjour romantique à l'hôtel avec des prix que vous allez adorer.</p>
-          <button className="bg-colorB text-white py-2 px-6 rounded-lg">
-            Rechercher
-          </button>
+        <div className="absolute inset-0 flex flex-col justify-center items-start text-start text-white px-4 md:ml-10">
+          <h2 className="text-xl md:text-3xl font-bold mb-4">Allez plus loin, rapprochez-vous</h2>
+          <p className="text-sm md:text-lg mb-6">Éclipsez-vous pour un séjour romantique à l'hôtel avec des prix que vous allez adorer.</p>
+          <Buttons textContent="Rechercher" 
+          aria-label="Bouton de recherche" />
         </div>
 
     </div>
