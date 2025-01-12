3e93e68de5e8ee46e78b697ef6dde04444f59a5b
CecileYhy <gastrin.ce@gmail.com>
2025-01-12 21:26:44 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ import FetchFlights from "./components/FetchFlights";
 import Enseignes from './components/Enseignes';
 import HotelPays from './components/HotelPays';
 import Banniere from './components/Banniere';
-import BannierReservHotel from './components/BannierReservHotel';
 
 
 function App() {
@@ -66,7 +65,6 @@ function App() {
           <Route path="/Enseignes" element={<Enseignes />} />
           <Route path="/HotelPays" element={<HotelPays />} />
           <Route path="/Banniere" element={<Banniere />} />
-          <Route path='BannierReservHotel' element={<BannierReservHotel />}/>
 
         </Routes>
       </div>
