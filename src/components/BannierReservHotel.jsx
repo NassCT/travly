48616a49aa06c038be306c78d002fb8e6a710df9
Muhammad Shahid <contact@mshahid.fr>
2025-01-13 09:11:50 +0100

    BannierReservHotel v-2

--- src/components/BannierReservHotel.jsx
+++ src/components/BannierReservHotel.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import Buttons from './Buttons'
 
-function BannierReservHotel() {
-  return (
-    <section className='relative p-6 m-10'>
+function BannierReservHotel({
+  classSectionBannier ="relative p-6 m-10",
+  classBannier = "bg-colorB text-white p-8 rounded-lg w-full mx-auto",
+  classBannierHeading = "text-4xl font-bold pb-4",
+  classBannierContent = "w-8/12 mb-4",
 
-        <div className='bg-colorB text-white p-8 rounded-lg w-full mx-auto'>
-            <h1 className='text-5xl font-semibold mb-4'>
+}) {
+  return (
+    <section className={classSectionBannier}>
+
+        <div className={classBannier}>
+            <h1 className={classBannierHeading}>
                 Économisez sur votre prochaine réservation d'hôtel
             </h1>
-            <div className='max-w-lg mb-4'>
+            <div className={classBannierContent}>
 
-            <p className='text-sm mb-6'>
-            Nous avons soigneusement sélectionné pour vous les offres 
-            d'hôtels les plus intéressantes. Trouvez la chambre parfaite 
-            à un prix encore plus avantageux.
-            </p>
+              <p className='text-xl mb-6'>
+              Nous avons soigneusement sélectionné pour vous les offres 
+              d'hôtels les plus intéressantes. Trouvez la chambre parfaite 
+              à un prix encore plus avantageux.
+              </p>
             </div>
             <Buttons textContent="Rechercher" 
-          aria-label="Bouton de recherche"/>
+          aria-label="Bouton de recherche"
+          buttonClass="opacity-100 hover:opacity-50"/>
         </div>
 
     </section>
