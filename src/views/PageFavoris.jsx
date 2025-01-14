2bdc0020ab80f93f4a80de8097097232ba206d29
CecileYhy <gastrin.ce@gmail.com>
2025-01-14 13:12:14 +0100

    modif cardFav et PageFav

--- src/views/PageFavoris.jsx
+++ src/views/PageFavoris.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import CardFavoris from '../components/CardFavoris';
 import Footer from '../components/Footer';
+import Buttons from '../components/Buttons';
 
 function PageFavoris() {
-  return (
-    <section className='mt-6'>
 
-    <h1 className="text-3xl font-bold text-center">
-      Favoris
+  return (
+    <section className='mt-4'>
+
+    <div
+    onClick={() => window.history.back()} 
+    className='flex gap-2 pl-10 pb-4'>
+        <Buttons 
+        textContent="Retour" 
+        buttonClass={"px-10 border-1 border-colorW hover:bg-colorG"} />
+    </div>
+
+    <h1 className="text-3xl font-bold text-start ml-12">
+      Vos favoris
     </h1>
 
     <CardFavoris />
@@ -17,4 +27,4 @@ function PageFavoris() {
   )
 }
 
-export default PageFavoris
+export default PageFavoris;
