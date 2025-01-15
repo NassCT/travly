8e460fd31d3aa500d5e9c1edb8a86e6f9372976f
Muhammad Shahid <contact@mshahid.fr>
2025-01-15 15:15:14 +0100

    pageHotel avec l'images + contenu

--- src/components/CardFavoris.jsx
+++ src/components/CardFavoris.jsx
@@ -1,142 +1,102 @@
-import React, { useState } from 'react';
-import Icons from './Icons';
+import React, { useState } from "react";
+import Icons from "./Icons";
 
 function CardFavoris({
-    rating = "4",
-    image1Url = "../src/assets/images/pool-hotel.jpg", // image example pour un card
-    iconHeartFilled = "heart",
-    iconClassHeartFilled = "fill-colorG pl-1 cursor-pointer",
-    iconHeartEmpty = "heartempty",
-    iconClassHeartEmpty = "fill-colorG pl-1 cursor-pointer",
-    iconNameStar = "star",
-    iconSizeStar = "20px",
-    iconClassStar = "",
-    iconNameEmpty = "starempty",
-    iconSizeStarEmpty = "20px",
-    iconClassStarEmpty = "",
-    cardTitle = "Hôtel Contenental",
-    distance = "0.17 km",
-    comments = 252,
-    stayDates = "10 déc. - 29 décembre 2024",
-    nbAdults = 2,
-    adults = "Adults",
-    nbRooms = 1,
-    rooms = "Chambre",
-    price = "",
-    perNight = "",
+  rating = "4",
+  image1Url = "../src/assets/images/pool-hotel.jpg",
+  iconHeartFilled = "heart",
+  iconClassHeartFilled = "fill-colorG pl-1 cursor-pointer",
+  iconHeartEmpty = "heartempty",
+  iconClassHeartEmpty = "fill-colorG pl-1 cursor-pointer",
+  iconNameStar = "star",
+  iconSizeStar = "20px",
+  iconClassStar = "",
+  iconNameEmpty = "starempty",
+  iconSizeStarEmpty = "20px",
+  iconClassStarEmpty = "",
+  cardTitle = "Hôtel Contenental",
+  distance = "0.17 km",
+  comments = 252,
+  stayDates = "10 déc. - 29 décembre 2024",
+  nbAdults = 2,
+  adults = "Adults",
+  nbRooms = 1,
+  rooms = "Chambre",
+  price = "",
+  perNight = "",
 }) {
 
-    const hotelCards = [
-        {
-            id: 1,
-            isFavorite: false,
-        },
-        {
-            id: 2,
-            isFavorite: false,
-        },
-        {
-            id: 3,
-            isFavorite: false,
-        },
-        {
-            id: 4,
-            isFavorite: false,
-        },
-        {
-            id: 5,
-            isFavorite: false,
-        },
-        {
-            id: 6,
-            isFavorite: false,
-        },
-    ]
-
-    const [hotels, setHotels] = useState(hotelCards);
-
-    const toggleFavorite = (id) => {
-        setHotels((hotels) => 
-            hotels.map((hotel) => 
-                hotel.id === id ? { ...hotel, isFavorite: !hotel.isFavorite } : hotel
-            )
-        );
-    }
+  const [isFavorite, setIsFavorite] = useState(false);
+  const toggleFavorite = () => {
+    setIsFavorite((prevState) => !prevState);
+  }
   return (
-    <div className='flex flex-wrap g justify-between p-12 '>
-        
-        {hotels.map((hotel) => (
+    <div className="flex flex-wrap g justify-between px-6 py-6">
 
-        
+        <div className="bg-colorW shadow-md mb-5 pb-4 rounded-lg overflow-hidden w-full">
+          <img
+            src={image1Url}
+            alt={cardTitle}
+            className="w-full h-[200px] object-cover rounded-md"/>
+          <div className="flex justify-between items-center mt-4 px-4">
+            <h3 className="text-xl font-bold">{cardTitle}</h3>
+            <div onClick={toggleFavorite}>
+              <Icons
+                iconName={isFavorite ? iconHeartFilled : iconHeartEmpty}
+                iconClass={isFavorite ? iconClassHeartFilled : iconClassHeartEmpty}
+              />
+            </div>
 
-        <div key={hotel.id} className="bg-colorW shadow-md mb-5 pb-4 rounded-lg overflow-hidden w-full sm:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)]">
-
-            <img
-                src={image1Url}
-                alt={cardTitle}
-                className="w-full h-50 object-cover rounded-md"
-            />
+          </div>
 
 
-            <div className="flex justify-between items-center mt-4 px-4">
-                <h3 className="text-xl font-bold">{cardTitle}</h3>
-                <div onClick={() => toggleFavorite(hotel.id)}>
+          <p className=" text-colorGr px-4">
+            <span>{distance} du centre ville</span>
+          </p>
+
+
+          <div className="flex items-center mt-2 px-4">
+            {[...Array(5)].map((_, index) =>
+              index < rating ? (
                 <Icons
-                    iconName={hotel.isFavorite ? iconHeartFilled: iconHeartEmpty }
-                    iconClass={hotel.isFavorite ? iconClassHeartFilled : iconClassHeartEmpty }
-                    // onClick={() => toggleFavorite(hotel.id)}
+                  key={`star-${index}`}
+                  iconName={iconNameStar}
+                  iconClass={`${iconClassStar} fill-colorG`}
+                  iconSize={iconSizeStar}
                 />
-                </div>
+              ) : (
+                <Icons
+                  key={`empty-${index}`}
+                  iconName={iconNameEmpty}
+                  iconClass={`${iconClassStarEmpty} fill-colorG`}
+                  iconSize={iconSizeStarEmpty}
+                />
+              )
+            )}
 
-            </div>
-
-
-            <p className=" text-colorGr px-4">
-                <span >{distance} du centre ville</span>
+            <p className="ml-2">
+              <span>{comments}</span> Comments
             </p>
+          </div>
+
+   
+          <p className="text-colorGr mt-2 px-4">{stayDates}</p>
 
 
-            <div className="flex items-center mt-2 px-4">
-            {[...Array(5)].map((_, index) => (
-                index < rating ? (
-                    <Icons
-                    key={`star-${index}`}
-                    iconName={iconNameStar}
-                    iconClass={`${iconClassStar} fill-colorG`}
-                    iconSize={iconSizeStar}
-                    />
-                ) : (                
-                    <Icons
-                    key={`empty-${index}`}
-                    iconName={iconNameEmpty}
-                    iconClass={`${iconClassStarEmpty} fill-colorG`}
-                    iconSize={iconSizeStarEmpty}
-                    />
-                )
-            ))}
-
-                <p className="ml-2">
-                <span>{comments}</span> Comments
-                </p>
-            </div>
-
-
-            <p className="text-colorGr mt-2 px-4">{stayDates}</p>
-
-
-            <div className="flex justify-between mt-2 mb-2 px-4">
-                <p>
-                <span>{nbAdults}</span> {adults}
-                </p>
-                <p>
-                <span>{nbRooms}</span> {rooms}
-                </p>
-            </div>
-            <p className='text-right  right-4 bottom-4 pr-2'>
-                <span className='font-bold'>{price}</span> {perNight}
+          <div className="flex justify-between mt-2 mb-2 px-4">
+            <p>
+              <span>{nbAdults}</span> {adults}
             </p>
-        </div>    
-    ))}
+            <p>
+              <span>{nbRooms}</span> {rooms}
+            </p>
+          </div>
+          <p className="text-right  right-4 bottom-4 pr-2">
+            <span className="font-bold">{price}</span> {perNight}
+          </p>
+
+        </div>
+
 
     </div>
   );
--- src/views/PageHotel.jsx
+++ src/views/PageHotel.jsx
@@ -10,12 +10,12 @@ import SearchBar from '../components/SearchBar';
 function PageHotel() {
 
     const sixButtons = [
-        "Nice",
+        "Paris",
         "Toulouse",
         "Lille",
         "Lyon",
         "Strasbourg",
-        "Paris",
+        "Tous",
     ]
 
   return (
@@ -26,7 +26,7 @@ function PageHotel() {
 
         <BannierReservHotel />
         <section className=''>
-            <div className='flex flex-wrap gap-4 m-4'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-6 p-8'>
                 {sixButtons.map((ville, index) => (
                     <Buttons key={index} textContent={ville}
 
@@ -39,16 +39,133 @@ function PageHotel() {
                 ))}
             </div>
 
-            <div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 py-4'>
                 <CardFavoris
-                stayDates=''
-                nbAdults=''
-                adults=''
-                rooms=''
-                nbRooms=''
-                price='120€'
-                perNight=' / Nuit'
+                  rating = "4"
+                  image1Url = "../src/assets/images/hotels/pool-hotel.jpg" 
+
+                  cardTitle = "Hôtel Contenental Strasbourg"
+                  distance = "0.17 km"
+                  comments = "252"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "750€"
+                  perNight = " / par nuit"
                  />
+                <CardFavoris
+                  rating = "4"
+                  image1Url = "../src/assets/images//hotels/fourSeasonHotel.jpg" 
+
+                  cardTitle = "Four Seasons Hotel George"
+                  distance = "0.15 km"
+                  comments = "245"
+                  nbAdults = "1-2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "350€"
+                  perNight = " / par nuit"
+                 />
+                <CardFavoris
+                  rating = "4"
+                  image1Url = "../src/assets/images/hotels/zannier.jpeg" 
+
+                  cardTitle = "Zannier Le Chalet"
+                  distance = "0.10 km"
+                  comments = "852"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "750€"
+                  perNight = " / par nuit"
+                 />
+                <CardFavoris
+                  rating = "5"
+                  image1Url = "../src/assets/images/hotels/hotelInterieur.png" 
+
+                  cardTitle = "Hôtel Contenental Paris"
+                  distance = "0.17 km"
+                  comments = "252"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "450€"
+                  perNight = " / par nuit"
+                 />
+                <CardFavoris
+                  rating = "4"
+                  image1Url = "../src/assets/images/hotels/courbeau.jpg" 
+
+                  cardTitle = "Hôtel Cour du Corbeau"
+                  distance = "0.17 km"
+                  comments = "552"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "550€"
+                  perNight = " / par nuit"
+                 />
+                <CardFavoris
+                  rating = "4"
+                  image1Url = "../src/assets/images/hotels/carcassonne.jpg" 
+
+                  cardTitle = "Hôtel de la Cité Carcassonne"
+                  distance = "0.07 km"
+                  comments = "352"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "570€"
+                  perNight = " / par nuit"
+                 />
+                <CardFavoris
+                  rating = "4"
+                  image1Url = "../src/assets/images/hotels/vieParisien.jpg" 
+                  cardTitle = "La Venise Parisienne"
+                  distance = "0.17 km"
+                  comments = "650"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "460€"
+                  perNight = " / par nuit"
+                 />
+                <CardFavoris
+                  rating = "4"
+                  image1Url = "../src/assets/images/hotels/chambre-hotel.png" 
+
+                  cardTitle = "Hôtel 5 étoiles"
+                  distance = "0.17 km"
+                  comments = "252"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "480€"
+                  perNight = " / par nuit"
+                 />
+                <CardFavoris
+                  rating = "4"
+                  image1Url = "../src/assets/images/hotels/negresco.jpg" 
+
+                  cardTitle = "Le Negresco"
+                  distance = "0.17 km"
+                  comments = "455"
+                  nbAdults = "2"
+                  adults = "Adults"
+                  nbRooms = "1"
+                  rooms = "Chambre"
+                  price = "350€"
+                  perNight = " / par nuit"
+                 />
+
             </div>
 
 
